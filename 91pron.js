a28e8be1b614ae94e7e18a0363c338d689ef8924
TOM <89062432+tom-proxy@users.noreply.github.com>
2025-07-07 00:20:54 +0800

    Update 91pron.js

--- 91pron.js
+++ 91pron.js
@@ -1,30 +1,5 @@
 const WidgetMetadata = {
-  id: "91porn",
-  title: "91Porn",
-  description: "获取 91Porn 视频资源",
-  author: "🅣🅞🅜",  // 如果您有特别署名，请告诉我
-  site: "https://91porn.com",
-  version: "0.0.1",
-  requiredVersion: "0.5.0",
-  detailCacheDuration: 60,
-  modules: [
-    {
-      title: "最近加精",
-      description: "获取 91Porn 视频",
-      requiresWebView: false,
-      functionName: "getList",  // 必须与脚本中实际存在的函数匹配
-      cacheDuration: 3600,
-      params: [
-        {
-          name: "from",
-          title: "页码",
-          type: "page",
-          description: "页码",
-          value: "1"
-        }
-      ]
-    }
-  ]
+  author: "🅣🅞🅜"
 };
 function listCacheClear() {
     this.__data__ = [];
