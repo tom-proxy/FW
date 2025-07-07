ae3c77aabe3546929e4c8c10874da28c41e1385e
TOM <89062432+tom-proxy@users.noreply.github.com>
2025-07-07 16:54:31 +0800

    Update xjlive.js

--- xjlive.js
+++ xjlive.js
@@ -1,38 +1,42 @@
-// 聚合直播 Forward 专用版
-const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0.1 Mobile/15E148 Safari/604.1';
-
 const site = 'http://api.maiyoux.com:81';
 const jsonUrl = `${site}/mf/json.txt`;
+const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0.1 Mobile/15E148 Safari/604.1';
 
+// 获取频道列表
 async function getList() {
   const res = await $http.get({ url: jsonUrl, header: { 'User-Agent': UA } });
   const data = res.data || {};
-  const ignore = ['卫视直播', '龙珠', '映客'];
   const list = [];
 
-  (data.pingtai || []).forEach(e => {
+  if (!data.pingtai || !Array.isArray(data.pingtai)) return list;
+
+  const ignore = ['卫视直播', '龙珠', '映客'];
+
+  for (const e of data.pingtai) {
     const name = `${e.title}(${e.Number})`;
-    if (ignore.some(v => name.includes(v))) return;
+    if (ignore.some(v => name.includes(v))) continue;
     list.push({
       name,
       ext: {
         url: e.address
       }
     });
-  });
+  }
 
   return list;
 }
 
+// 获取频道内节目列表
 async function getVodList(ext) {
-  const { url, page } = ext;
-  const realUrl = `${site}/mf/${url}`;
-  const res = await $http.get({ url: realUrl, header: { 'User-Agent': UA } });
+  const url = `${site}/mf/${ext.url}`;
+  const res = await $http.get({ url, header: { 'User-Agent': UA } });
   const data = res.data || {};
   const list = [];
 
-  (data.zhubo || []).forEach(e => {
-    if (e.address && e.address.startsWith('rtmp')) return;
+  if (!data.zhubo || !Array.isArray(data.zhubo)) return { list };
+
+  for (const e of data.zhubo) {
+    if (e.address && e.address.startsWith('rtmp')) continue;
     list.push({
       vod_id: e.address,
       vod_name: e.title,
@@ -42,22 +46,21 @@ async function getVodList(ext) {
         url: e.address
       }
     });
-  });
+  }
 
-  return {
-    list
-  };
+  return { list };
 }
 
+// 获取节目详情
 async function loadDetail(ext) {
   const url = ext.url;
   return {
     list: [
       {
-        title: "默认分组",
+        title: '默认分组',
         tracks: [
           {
-            name: "播放",
+            name: '播放',
             ext: {
               playurl: url
             }
@@ -68,6 +71,7 @@ async function loadDetail(ext) {
   };
 }
 
+// 获取播放链接
 async function getPlayInfo(ext) {
   return {
     urls: [ext.playurl]
